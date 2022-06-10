b8581b477f75ef950f2bb0e957b51b1ea5257c3e
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-10 19:43:27 +0300

    feat: post page

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
             $select_all_posts_query = mysqli_query($connection, $query);
 
             while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                $post_id = $row['post_id'];
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
@@ -28,7 +29,7 @@
                 $post_content = $row['post_content'];
             ?>
                 <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
                     by
@@ -44,7 +45,7 @@
                 <p>
                     <?php echo $post_content; ?>
                 </p>
-                <a class="btn btn-primary" href="#">
+                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">
                     Read More
                     <span class="glyphicon glyphicon-chevron-right"></span>
                 </a>
